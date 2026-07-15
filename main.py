4fb905fcb59f8fb67f421aa244c50dabd7feadd3
Wann1904 <iwan.vj421@example.com>
2026-07-16 01:52:50 +0700

    ganti text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 TOKEN = "8881078652:AAEj-804xIW7kZiWA8nQW3yAZBc06O4R9QI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Halo Wan! Bot berhasil jalan 🚀")
+    await update.message.reply_text("Bot berhasil jalan 🚀")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
